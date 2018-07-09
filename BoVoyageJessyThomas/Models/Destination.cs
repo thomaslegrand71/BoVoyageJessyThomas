610c17d4232e261b52a99d477ebeee6d91d6bafb
Fati <Adminl@EMER06>
2018-07-09 18:34:42 +0200

    création du projet

---
+++ BoVoyageJessyThomas/Models/Destination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageJessyThomas.Models
+{
+    public class Destination
+    {
+        public string Continent { get; set; }
+
+        public string Pays { get; set; }
+
+        public string Region { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
